f0248a144c1890bc45071422c8358048d4d47380
fox <fox754110@gmail.com>
2021-07-11 23:49:43 +0300

    changed import

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { sayHello } from '../src/cli.js';
-import brainEven from '../src/games/even.js';
+import { brainEven } from '../src/games/even.js';
 
 console.log('Welcome to the Brain Games!');
 sayHello();
