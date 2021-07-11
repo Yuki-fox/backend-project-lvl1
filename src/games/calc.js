930d81984d9926b122e7f099a71c11c81fd61a90
fox <fox754110@gmail.com>
2021-07-12 01:45:28 +0300

    refactioring of returning parameters

--- src/games/calc.js
+++ src/games/calc.js
@@ -13,15 +13,13 @@ function multiplication(a, b) {
 const operators = [adition, substraction, multiplication];
 const operatorIcons = ['+', '-', '*'];
 function calcCore() {
-  const result = [];
   const numberOne = randomNumber(0, 100);
   const numberTwo = randomNumber(0, 20);
   const operatorRandom = randomNumber(0, 2);
   const currentOperator = operators[operatorRandom];
   const answer = readlineSync.question(`Question: ${numberOne} ${operatorIcons[operatorRandom]} ${numberTwo}\n`);
   const answerCheck = String(currentOperator(numberOne, numberTwo));
-  result.push(answer, answerCheck);
-  return result;
+  return [answer, answerCheck];
 }
 function brainCalc() {
   console.log('What is the result of the expression?');
--- src/games/even.js
+++ src/games/even.js
@@ -5,13 +5,11 @@ function isEven(number) {
   return (number % 2 === 0);
 }
 function brainEvenCore() {
-  const result = [];
   const playableNumber = randomNumber(1, 100);
   const evenCheck = isEven(playableNumber);
   const answer = readlineSync.question(`Question: ${playableNumber}\n`);
   const answerCheck = evenCheck ? 'yes' : 'no';
-  result.push(answer, answerCheck);
-  return result;
+  return [answer, answerCheck];
 }
 function brainEven() {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
